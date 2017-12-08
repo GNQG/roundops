326eb2f5b5529715b92f97e4b53af1c5beaa9b8e
GNQG <my.t.gnq@gmail.com>
2017-12-09 00:30:05 +0900

    separated Emulation into EmulationRegular(use safetwoproduct_branch) and EmulationFma(use safetwoproduct_fma)

--- src/methods/emulation.rs
+++ src/methods/emulation.rs
@@ -3,235 +3,280 @@ use core::marker::PhantomData;
 use float_traits::IEEE754Float;
 
 use roundops::*;
-use super::safeeft::{safetwosum_straight as twosum, safetwoproduct_branch as twoproduct};
+use super::safeeft::{safetwosum_straight as safetwosum, safetwoproduct_branch};
+#[cfg(feature = "use-fma")]
+use super::safeeft::safetwoproduct_fma;
+#[cfg(feature = "use-fma")]
+use super::fma::{fma, Fma};
+
 use utils::FloatSuccPred;
 
-pub struct Emulation<T>(PhantomData<fn(T)>);
+pub struct EmulationRegular<T>(PhantomData<fn(T)>);
+#[cfg(feature = "use-fma")]
+pub struct EmulationFma<T>(PhantomData<fn(T)>);
 
-impl<T: IEEE754Float + Clone> RoundAdd for Emulation<T> {
-    type Num = T;
-    fn add_up(a: T, b: T) -> T {
-        let (x, y) = twosum(a.clone(), b.clone());
-        if x == T::infinity() {
-            x
-        } else if x == T::neg_infinity() {
-            if a == T::neg_infinity() || b == T::neg_infinity() {
-                x
-            } else {
-                T::min_value()
-            }
-        } else {
-            if y > T::zero() { x.succ() } else { x }
-        }
-    }
-    fn add_down(a: T, b: T) -> T {
-        let (x, y) = twosum(a.clone(), b.clone());
-        if x == T::infinity() {
-            if a == T::neg_infinity() || b == T::neg_infinity() {
-                x
-            } else {
-                T::max_value()
-            }
-        } else if x == T::neg_infinity() {
-            x
-        } else {
-            if y < T::zero() { x.pred() } else { x }
-        }
-    }
-}
-
-impl<T: IEEE754Float + Clone> RoundSub for Emulation<T> {
-    type Num = T;
-    #[inline]
-    fn sub_up(a: T, b: T) -> T {
-        Self::add_up(a, -b)
-    }
-    #[inline]
-    fn sub_down(a: T, b: T) -> T {
-        Self::add_down(a, -b)
-    }
-}
-
-impl<T: IEEE754Float + Clone> RoundMul for Emulation<T> {
-    type Num = T;
-    fn mul_up(a: T, b: T) -> T {
-        let (x, y) = twoproduct(a.clone(), b.clone());
-        if x == T::infinity() {
-            x
-        } else if x == T::neg_infinity() {
-            if a == T::neg_infinity() || b == T::neg_infinity() {
-                x
-            } else {
-                T::min_value()
-            }
-        } else {
-            if x.clone().abs() > T::min_positive() / T::eps() * T::radix() {
-                if y > T::zero() { x.succ() } else { x }
-            } else {
-                let (s_h, s_l) = twoproduct(a * T::unit_underflow().sqrt(),
-                                            b * T::unit_underflow().sqrt()); // TODO: check
-                let t = (x.clone() * T::unit_underflow().sqrt()) * T::unit_underflow().sqrt();
-                if t < s_h || (t == s_h && s_l > T::zero()) {
-                    x.succ()
-                } else {
+macro_rules! impl_rops {
+    ($bound:ident $(+$bound1:ident)+, $method:ident, $twoproduct:ident) => (
+        impl<T: $($bound1+)+$bound> RoundAdd for $method<T> {
+            type Num = T;
+            fn add_up(a: T, b: T) -> T {
+                let (x, y) = safetwosum(a.clone(), b.clone());
+                if x == T::infinity() {
                     x
-                }
-            }
-        }
-    }
-    fn mul_down(a: T, b: T) -> T {
-        let (x, y) = twoproduct(a.clone(), b.clone());
-        if x == T::infinity() {
-            if a == T::infinity() || b == T::infinity() {
-                // TODO: check
-                x
-            } else {
-                T::max_value()
-            }
-        } else if x == T::neg_infinity() {
-            x
-        } else {
-            if x.clone().abs() > T::min_positive() / T::eps() * T::radix() {
-                if y < T::zero() { x.pred() } else { x }
-            } else {
-                let (s_h, s_l) = twoproduct(a * T::unit_underflow().sqrt(),
-                                            b * T::unit_underflow().sqrt());
-                let t = (x.clone() * T::unit_underflow().sqrt()) * T::unit_underflow().sqrt();
-                if t > s_h || (t == s_h && s_l < T::zero()) {
-                    x.pred()
-                } else {
-                    x
-                }
-            }
-        }
-    }
-}
-
-impl<T: IEEE754Float + Clone> RoundDiv for Emulation<T> {
-    type Num = T;
-    fn div_up(a: T, b: T) -> T {
-        if a == T::zero() || b == T::zero() || a.clone().abs() == T::infinity() ||
-           b.clone().abs() == T::infinity() || a != a || b != b {
-            a / b
-        } else {
-            let (mut ss, mut bb) = (a, b);
-            if bb < T::zero() {
-                ss = ss * -T::one();
-                bb = bb * -T::one();
-            }
-            if ss.clone().abs() < T::min_positive() / T::eps() * T::radix() {
-                if bb.clone().abs() < T::min_positive() / T::eps() / T::eps() {
-                    ss = ss * T::radix() * T::eps() * T::eps();
-                    bb = bb * T::radix() * T::eps() * T::eps();
-                } else {
-                    if ss < T::zero() {
-                        return T::zero();
+                } else if x == T::neg_infinity() {
+                    if a == T::neg_infinity() || b == T::neg_infinity() {
+                        x
                     } else {
-                        return T::unit_underflow();
+                        T::min_value()
+                    }
+                } else {
+                    if y > T::zero() { x.succ() } else { x }
+                }
+            }
+            fn add_down(a: T, b: T) -> T {
+                let (x, y) = safetwosum(a.clone(), b.clone());
+                if x == T::infinity() {
+                    if a == T::neg_infinity() || b == T::neg_infinity() {
+                        x
+                    } else {
+                        T::max_value()
+                    }
+                } else if x == T::neg_infinity() {
+                    x
+                } else {
+                    if y < T::zero() { x.pred() } else { x }
+                }
+            }
+        }
+
+        impl<T: $($bound1+)+$bound> RoundSub for $method<T> {
+            type Num = T;
+            #[inline]
+            fn sub_up(a: T, b: T) -> T {
+                Self::add_up(a, -b)
+            }
+            #[inline]
+            fn sub_down(a: T, b: T) -> T {
+                Self::add_down(a, -b)
+            }
+        }
+
+        impl<T: $($bound1+)+$bound> RoundMul for $method<T> {
+            type Num = T;
+            fn mul_up(a: T, b: T) -> T {
+                let (x, y) = $twoproduct(a.clone(), b.clone());
+                if x == T::infinity() {
+                    x
+                } else if x == T::neg_infinity() {
+                    if a == T::neg_infinity() || b == T::neg_infinity() {
+                        x
+                    } else {
+                        T::min_value()
+                    }
+                } else {
+                    if x.clone().abs() > T::min_positive() / T::eps() * T::radix() {
+                        if y > T::zero() { x.succ() } else { x }
+                    } else {
+                        use num_traits::One;
+                        let (s_h, s_l) =
+                            $twoproduct(a *
+                                        T::radix_powi(T::bit_size() -
+                                                        (T::min_exponent() /
+                                                        (T::Exponent::one() + T::Exponent::one()))),
+                                        b *
+                                        T::radix_powi(T::bit_size() -
+                                                        (T::min_exponent() /
+                                                        (T::Exponent::one() + T::Exponent::one()))));
+                        let t = (x.clone() *
+                                T::radix_powi(T::bit_size() -
+                                            (T::min_exponent() /
+                                                (T::Exponent::one() + T::Exponent::one())))) *
+                                T::radix_powi(T::bit_size() -
+                                            (T::min_exponent() /
+                                            (T::Exponent::one() + T::Exponent::one())));
+                        if t < s_h || (t == s_h && s_l > T::zero()) {
+                            x.succ()
+                        } else {
+                            x
+                        }
                     }
                 }
             }
-            let d = ss.clone() / bb.clone();
-            if d.is_infinite() {
-                if d > T::zero() { d } else { T::min_value() }
-            } else {
-                let (x, y) = twoproduct(d.clone(), bb.clone());
-                if x < ss || (x == ss && y > T::zero()) {
-                    d.succ()
-                } else {
-                    d
-                }
-            }
-        }
-    }
-    fn div_down(a: T, b: T) -> T {
-        if a == T::zero() || b == T::zero() || a.clone().abs() == T::infinity() ||
-           b.clone().abs() == T::infinity() || a != a || b != b {
-            a / b
-        } else {
-            let (mut ss, mut bb) = (a, b);
-
-            if bb < T::zero() {
-                ss = ss * -T::one();
-                bb = bb * -T::one();
-            }
-            if ss.clone().abs() < T::min_positive() / T::eps() * T::radix() {
-                if bb.clone().abs() < T::min_positive() / T::eps() / T::eps() {
-                    ss = ss * T::radix() * T::eps() * T::eps();
-                    bb = bb * T::radix() * T::eps() * T::eps();
-                } else {
-                    if ss < T::zero() {
-                        return -T::unit_underflow();
+            fn mul_down(a: T, b: T) -> T {
+                let (x, y) = $twoproduct(a.clone(), b.clone());
+                if x == T::infinity() {
+                    if a == T::infinity() || b == T::infinity() {
+                        // TODO: check
+                        x
                     } else {
-                        return T::zero();
+                        T::max_value()
+                    }
+                } else if x == T::neg_infinity() {
+                    x
+                } else {
+                    if x.clone().abs() > T::min_positive() / T::eps() * T::radix() {
+                        if y < T::zero() { x.pred() } else { x }
+                    } else {
+                        use num_traits::One;
+                        let (s_h, s_l) =
+                            $twoproduct(a *
+                                        T::radix_powi(T::bit_size() -
+                                                        (T::min_exponent() /
+                                                        (T::Exponent::one() + T::Exponent::one()))),
+                                        b *
+                                        T::radix_powi(T::bit_size() -
+                                                        (T::min_exponent() /
+                                                        (T::Exponent::one() + T::Exponent::one()))));
+                        let t = (x.clone() *
+                                T::radix_powi(T::bit_size() -
+                                            (T::min_exponent() /
+                                                (T::Exponent::one() + T::Exponent::one())))) *
+                                T::radix_powi(T::bit_size() -
+                                            (T::min_exponent() /
+                                            (T::Exponent::one() + T::Exponent::one())));
+                        if t > s_h || (t == s_h && s_l < T::zero()) {
+                            x.pred()
+                        } else {
+                            x
+                        }
                     }
                 }
             }
-            let d = ss.clone() / bb.clone();
-            if d.is_infinite() {
-                if d > T::zero() { T::max_value() } else { d }
-            } else {
-                let (x, y) = twoproduct(d.clone(), bb.clone());
-                if x > ss || (x == ss && y < T::zero()) {
-                    d.pred()
+        }
+
+        impl<T: $($bound1+)+$bound> RoundDiv for $method<T> {
+            type Num = T;
+            fn div_up(a: T, b: T) -> T {
+                if a == T::zero() || b == T::zero() || a.clone().abs() == T::infinity() ||
+                b.clone().abs() == T::infinity() || a != a || b != b {
+                    a / b
                 } else {
-                    d
+                    let (mut ss, mut bb) = (a, b);
+                    if bb < T::zero() {
+                        ss = ss * -T::one();
+                        bb = bb * -T::one();
+                    }
+                    if ss.clone().abs() < T::min_positive() / T::eps() * T::radix() {
+                        if bb.clone().abs() < T::min_positive() / T::eps() / T::eps() {
+                            ss = ss * T::radix() * T::eps() * T::eps();
+                            bb = bb * T::radix() * T::eps() * T::eps();
+                        } else {
+                            if ss < T::zero() {
+                                return T::zero();
+                            } else {
+                                return T::unit_underflow();
+                            }
+                        }
+                    }
+                    let d = ss.clone() / bb.clone();
+                    if d.is_infinite() {
+                        if d > T::zero() { d } else { T::min_value() }
+                    } else {
+                        let (x, y) = $twoproduct(d.clone(), bb.clone());
+                        if x < ss || (x == ss && y > T::zero()) {
+                            d.succ()
+                        } else {
+                            d
+                        }
+                    }
+                }
+            }
+            fn div_down(a: T, b: T) -> T {
+                if a == T::zero() || b == T::zero() || a.clone().abs() == T::infinity() ||
+                b.clone().abs() == T::infinity() || a != a || b != b {
+                    a / b
+                } else {
+                    let (mut ss, mut bb) = (a, b);
+
+                    if bb < T::zero() {
+                        ss = ss * -T::one();
+                        bb = bb * -T::one();
+                    }
+                    if ss.clone().abs() < T::min_positive() / T::eps() * T::radix() {
+                        if bb.clone().abs() < T::min_positive() / T::eps() / T::eps() {
+                            ss = ss * T::radix() * T::eps() * T::eps();
+                            bb = bb * T::radix() * T::eps() * T::eps();
+                        } else {
+                            if ss < T::zero() {
+                                return -T::unit_underflow();
+                            } else {
+                                return T::zero();
+                            }
+                        }
+                    }
+                    let d = ss.clone() / bb.clone();
+                    if d.is_infinite() {
+                        if d > T::zero() { T::max_value() } else { d }
+                    } else {
+                        let (x, y) = $twoproduct(d.clone(), bb.clone());
+                        if x > ss || (x == ss && y < T::zero()) {
+                            d.pred()
+                        } else {
+                            d
+                        }
+                    }
                 }
             }
         }
-    }
+
+        impl<T: $($bound1+)+$bound> RoundSqrt for $method<T> {
+            fn sqrt_up(a: T) -> T {
+                let r = a.clone().sqrt();
+                if a < T::min_positive() / T::eps() * T::radix() {
+                    let (ss, rr) = (a * T::radix() / T::eps() * T::radix() / T::eps(),
+                                    r.clone() * T::radix() / T::eps());
+                    let (x, y) = $twoproduct(ss.clone(), rr.clone());
+                    if x < ss || (x == ss && y < T::zero()) {
+                        r.succ()
+                    } else {
+                        r
+                    }
+                } else {
+                    let (x, y) = $twoproduct(r.clone(), r.clone());
+                    if x < a || (x == a && y < T::zero()) {
+                        r.succ()
+                    } else {
+                        r
+                    }
+                }
+            }
+            fn sqrt_down(a: T) -> T {
+                let r = a.clone().sqrt();
+                if a < T::min_positive() / T::eps() * T::radix() {
+                    let (ss, rr) = (a * T::radix() / T::eps() * T::radix() / T::eps(),
+                                    r.clone() * T::radix() / T::eps());
+                    let (x, y) = $twoproduct(ss.clone(), rr.clone());
+                    if x > ss || (x == ss && y > T::zero()) {
+                        r.pred()
+                    } else {
+                        r
+                    }
+                } else {
+                    let (x, y) = $twoproduct(r.clone(), r.clone());
+                    if x > a || (x == a && y > T::zero()) {
+                        r.pred()
+                    } else {
+                        r
+                    }
+                }
+            }
+        }
+    )
 }
 
-impl<T: IEEE754Float + Clone> RoundSqrt for Emulation<T> {
-    fn sqrt_up(a: T) -> T {
-        let r = a.clone().sqrt();
-        if a < T::min_positive() / T::eps() * T::radix() {
-            let (ss, rr) = (a * T::radix() / T::eps() * T::radix() / T::eps(),
-                            r.clone() * T::radix() / T::eps());
-            let (x, y) = twoproduct(ss.clone(), rr.clone());
-            if x < ss || (x == ss && y < T::zero()) {
-                r.succ()
-            } else {
-                r
-            }
-        } else {
-            let (x, y) = twoproduct(r.clone(), r.clone());
-            if x < a || (x == a && y < T::zero()) {
-                r.succ()
-            } else {
-                r
-            }
-        }
-    }
-    fn sqrt_down(a: T) -> T {
-        let r = a.clone().sqrt();
-        if a < T::min_positive() / T::eps() * T::radix() {
-            let (ss, rr) = (a * T::radix() / T::eps() * T::radix() / T::eps(),
-                            r.clone() * T::radix() / T::eps());
-            let (x, y) = twoproduct(ss.clone(), rr.clone());
-            if x > ss || (x == ss && y > T::zero()) {
-                r.pred()
-            } else {
-                r
-            }
-        } else {
-            let (x, y) = twoproduct(r.clone(), r.clone());
-            if x > a || (x == a && y > T::zero()) {
-                r.pred()
-            } else {
-                r
-            }
-        }
-    }
-}
+impl_rops!(IEEE754Float + Clone,
+           EmulationRegular,
+           safetwoproduct_branch);
+#[cfg(feature = "use-fma")]
+impl_rops!(IEEE754Float + Fma + Clone, EmulationFma, safetwoproduct_fma);
 
 #[cfg(test)]
 mod tests {
-    use super::Emulation;
+    use super::EmulationRegular;
     use roundops::*;
     use super::FloatSuccPred;
 
-    type Emuf64 = Emulation<f64>;
+    type Emuf64 = EmulationRegular<f64>;
 
     #[test]
     fn addition() {
@@ -276,7 +321,7 @@ mod tests {
             use super::twoproduct;
             let (x, y) = (Emuf64::sqrt_up(a), Emuf64::sqrt_down(a));
             println!("{:e}, [{:e}, {:e}]", a.sqrt(), y, x);
-            println!("{:?}", twoproduct(a.sqrt(), a.sqrt()));
+            println!("{:?}", $twoproduct(a.sqrt(), a.sqrt()));
             assert!(x == y.succ() || x == y);
             assert!(y <= a.sqrt() && a.sqrt() <= x);
         }
--- src/methods/mod.rs
+++ src/methods/mod.rs
@@ -1,4 +1,6 @@
 extern crate safeeft;
+#[cfg(feature = "use-fma")]
+extern crate fma;
 
 mod hardware;
 mod emulation;
@@ -8,9 +10,18 @@ mod succpred_unchecked;
 mod roughwrap;
 mod roughwrap_unchecked;
 
-pub use self::emulation::Emulation;
+pub use self::emulation::EmulationRegular;
+#[cfg(feature = "use-fma")]
+pub use self::emulation::EmulationFma;
+#[cfg(not(feature = "use-fma"))]
+pub use self::EmulationRegular as Emulation;
+#[cfg(feature = "use-fma")]
+pub use self::EmulationFma as Emulation;
+
 pub use self::emulation_unchecked::EmulationUnchecked;
+
 pub use self::succpred::SuccPred;
 pub use self::succpred_unchecked::SuccPredUnchecked;
+
 pub use self::roughwrap::RoughWrapping;
 pub use self::roughwrap_unchecked::RoughWrappingUnchecked;
